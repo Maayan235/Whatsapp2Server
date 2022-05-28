0d26d7e49e3e7c5e047c43a17f08a6cc182c92dd
Maayan235 <maayansharabi2@gmail.com>
2022-05-28 23:00:03 +0300

    wwww

--- Whatsapp2Server/Hubs/ChatHub.cs
+++ Whatsapp2Server/Hubs/ChatHub.cs
@@ -7,24 +7,17 @@ namespace Whatsapp2Server.Hubs
     public class ChatHub : Hub
     {
 
-        private readonly IDictionary<string, string> _chats;
-
         public async Task SendMessage(string message)
         {
             await Clients.Group(message).SendAsync("ReceiveMessage", message);
 
-/*            if (_chats.TryGetValue(Context.ConnectionId, out string other))
-            {
-                await Clients.Group(other).SendAsync("ReceiveMessage", message);
-            }*/
         }
 
 
         public async Task joinToListeners([Bind("myId")] ChatObject chatObject)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, chatObject.myId);
-            _chats[Context.ConnectionId] = chatObject.myId;
-            //await Clients.Group(chatObject.myId).SendAsync("ReceiveMessage", chatObject.chatMember);
+
         }
     }
 }
